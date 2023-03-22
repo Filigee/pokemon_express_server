d7ba2b7f60f4eebbdc488979b7a3224062c72235
Bradley Hagger <brad_2nd@hotmail.com>
2023-03-22 17:02:43 +0000

    Changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const PORT = 8000
+const PORT = 3000
 
 const pokemon = {
     "charmander": {
@@ -39,5 +39,5 @@ app.get("/api/:pokemon", (req, res) => {
 })
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log("Listening on port 8000")
+    console.log("Listening on port 3000")
 })
